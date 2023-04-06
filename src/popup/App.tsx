e49956c4ab1cf8fe8876dba9ed23cf2a76138ac5
Zaw <zawlinnnaing0018@gmail.com>
2023-04-06 15:38:09 +0700

    refactor: remove index css

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -8,6 +8,7 @@ import {
 } from '../util/host'
 import './App.css'
 import { Link } from '@mui/material'
+import AppThemeProvider from '../providers/APpThemeProvider'
 
 function useIsDomainBlock(): boolean {
   const [isDomainBlocked, setIsDomainBlocked] = useState(false)
@@ -60,28 +61,30 @@ function App(): JSX.Element {
   }
 
   return (
-    <div className="App">
-      <h1>Zen mode extension</h1>
-      <div className="card">
-        <button disabled={loading || isDomainBlocked} onClick={blockWebsite}>
-          {isDomainBlocked ? 'You cannot block this site': 'Block this site'}
-        </button>
+    <AppThemeProvider>
+      <div className="App">
+        <h1>Zen mode extension</h1>
+        <div className="card">
+          <button disabled={loading || isDomainBlocked} onClick={blockWebsite}>
+            {isDomainBlocked ? 'You cannot block this site' : 'Block this site'}
+          </button>
 
-        <Link
-          style={{
-            color: 'gray',
-            textDecorationColor: 'gray',
-            cursor: 'pointer',
-          }}
-          marginY={5}
-          display="block"
-          variant="subtitle2"
-          onClick={goToOptionsPage}
-        >
-          Go to options page
-        </Link>
+          <Link
+            style={{
+              color: 'gray',
+              textDecorationColor: 'gray',
+              cursor: 'pointer',
+            }}
+            marginY={5}
+            display="block"
+            variant="subtitle2"
+            onClick={goToOptionsPage}
+          >
+            Go to options page
+          </Link>
+        </div>
       </div>
-    </div>
+    </AppThemeProvider>
   )
 }
 
