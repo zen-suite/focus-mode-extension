fbf7a6d6e5da3094bca22278bfd0aca961deae56
Zaw Lin Naing <zawlinnnaing0018@gmail.com>
2023-04-24 23:24:02 +0700

    fix: popup block button not reflect the status

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,88 +1,16 @@
-import { Button, CssBaseline, Link } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { addBlockedSite, findBlockedSiteByDomain } from '../domain/block-site'
+import { CssBaseline } from '@mui/material'
 import AppThemeProvider from '../providers/AppThemeProvider'
-import { getHostDomain, getHostUrl, isHttpProtocol } from '../util/host'
+import { BlockedSitesProvider } from '../providers/BlockedSitesProvider'
 import './App.css'
-
-function useIsDomainBlock(): boolean {
-  const [isDomainBlocked, setIsDomainBlocked] = useState(false)
-
-  async function updateIsDomainBlocked(): Promise<void> {
-    const currentDomain = await getHostDomain()
-    const isHostHttpProtocol = isHttpProtocol(await getHostUrl())
-    if (!isHostHttpProtocol || !currentDomain) {
-      setIsDomainBlocked(true)
-      return
-    }
-    const blockedSite = await findBlockedSiteByDomain(currentDomain)
-    setIsDomainBlocked(!!blockedSite)
-  }
-
-  useEffect(() => {
-    updateIsDomainBlocked()
-      .then(() => {})
-      .catch(() => {})
-  }, [])
-
-  return isDomainBlocked
-}
+import AppContentContainer from './AppContentContainer'
 
 function App(): JSX.Element {
-  const [loading, setLoading] = useState(false)
-
-  const isDomainBlocked = useIsDomainBlock()
-  async function blockWebsite(): Promise<void> {
-    try {
-      setLoading(true)
-      const currentDomain = await getHostDomain()
-      if (!currentDomain) {
-        return
-      }
-
-      await addBlockedSite(currentDomain)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  function goToOptionsPage() {
-    if (chrome.runtime.openOptionsPage) {
-      chrome.runtime.openOptionsPage()
-    } else {
-      window.open(chrome.runtime.getURL('options.html'))
-    }
-  }
-
   return (
     <AppThemeProvider>
-      <CssBaseline enableColorScheme />
-      <div className="App">
-        <h1>Zen mode extension</h1>
-        <div className="card">
-          <Button
-            disabled={loading || isDomainBlocked}
-            onClick={blockWebsite}
-            variant="outlined"
-          >
-            {isDomainBlocked ? 'You cannot block this site' : 'Block this site'}
-          </Button>
-
-          <Link
-            style={{
-              color: 'gray',
-              textDecorationColor: 'gray',
-              cursor: 'pointer',
-            }}
-            marginY={5}
-            display="block"
-            variant="subtitle2"
-            onClick={goToOptionsPage}
-          >
-            Go to options page
-          </Link>
-        </div>
-      </div>
+      <BlockedSitesProvider>
+        <CssBaseline enableColorScheme />
+        <AppContentContainer />
+      </BlockedSitesProvider>
     </AppThemeProvider>
   )
 }
---
+++ src/popup/AppContentContainer.tsx
@@ -0,0 +1,142 @@
+import { useEffect, useMemo, useState } from 'react'
+import { Button, Link, Typography } from '@mui/material'
+import { addBlockedSite, type IBlockedSite } from '../domain/block-site'
+import { useBlockedSites } from '../providers/BlockedSitesProvider'
+import { getHostDomain, getHostUrl, isHttpProtocol } from '../util/host'
+
+interface IInjectedProps {
+  blockedSites: IBlockedSite[]
+  blockSite: () => Promise<void>
+  validDomain: boolean
+  goToOptionsPage: () => void
+  currentDomain: string | undefined
+}
+
+function useIsDomainValid() {
+  const [validDomain, setIsDomainBlocked] = useState(false)
+
+  async function checkIsDomainValid(): Promise<void> {
+    const currentDomain = await getHostDomain()
+    const isHostHttpProtocol = isHttpProtocol(await getHostUrl())
+
+    setIsDomainBlocked(Boolean(currentDomain && isHostHttpProtocol))
+  }
+
+  useEffect(() => {
+    checkIsDomainValid()
+      .then(() => {})
+      .catch(() => {})
+  }, [])
+
+  return validDomain
+}
+
+export default function AppContentContainer() {
+  const { blockedSites, refetchBlockedSites } = useBlockedSites()
+  const [currentDomain, setCurrentDomain] = useState<string | undefined>()
+
+  useEffect(() => {
+    getHostDomain().then(setCurrentDomain)
+  }, [])
+
+  async function blockWebsite() {
+    if (!currentDomain) {
+      return
+    }
+    await addBlockedSite(currentDomain)
+    await refetchBlockedSites()
+  }
+
+  const validDomain = useIsDomainValid()
+
+  function goToOptionsPage() {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage()
+    } else {
+      window.open(chrome.runtime.getURL('options.html'))
+    }
+  }
+
+  return (
+    <AppContent
+      validDomain={validDomain}
+      blockedSites={blockedSites}
+      blockSite={blockWebsite}
+      goToOptionsPage={goToOptionsPage}
+      currentDomain={currentDomain}
+    />
+  )
+}
+
+export function AppContent(props: IInjectedProps) {
+  const [loading, setLoading] = useState(false)
+
+  const isDomainAlreadyBlocked = useMemo(() => {
+    return Boolean(
+      props.blockedSites.find(
+        (blockedSite) => blockedSite.domain === props.currentDomain
+      )
+    )
+  }, [props.blockedSites, props.currentDomain])
+
+  const buttonText = useMemo(() => {
+    if (isDomainAlreadyBlocked) {
+      return 'This website is already blocked'
+    }
+    if (!props.validDomain) {
+      return 'You cannot block this site'
+    }
+
+    return 'Block this website'
+  }, [isDomainAlreadyBlocked, props.validDomain])
+
+  async function blockSite() {
+    try {
+      setLoading(true)
+      await props.blockSite()
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Zen mode extension</h1>
+      <div className="card">
+        <Button
+          disabled={loading || !props.validDomain || isDomainAlreadyBlocked}
+          onClick={blockSite}
+          variant="outlined"
+        >
+          {buttonText}
+        </Button>
+        {isDomainAlreadyBlocked && (
+          <Typography
+            variant="caption"
+            display="block"
+            marginTop={2}
+            color="gray"
+          >
+            If you are still seeing the website, please try reloading it.
+          </Typography>
+        )}
+
+        <Link
+          style={{
+            textDecorationColor: 'gray',
+            cursor: 'pointer',
+          }}
+          marginY={5}
+          display="block"
+          variant="subtitle2"
+          onClick={props.goToOptionsPage}
+        >
+          Go to options page
+        </Link>
+      </div>
+    </div>
+  )
+}
