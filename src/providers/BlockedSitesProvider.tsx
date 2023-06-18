fc285940735b5e83be67ede4b5312fd9f7fbd174
Zaw <zawlinnnaing0018@gmail.com>
2023-06-18 13:52:01 +0700

    refactor: fix lint error

--- src/providers/BlockedSitesProvider.tsx
+++ src/providers/BlockedSitesProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react'
+import React, { useCallback, useContext, useEffect } from 'react'
 import {
   getBlockedSites,
   searchBlockSites,
